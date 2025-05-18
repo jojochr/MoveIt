b3269743e9e824a02cc207c85423157b3fd2a73e
Johannes Christel <johannes.christel@outlook.de>
2025-05-18 09:14:24 +0200

    Added input validation while typing, so nobody can just type letters in number only input

--- components/ExercisePageInput.tsx
+++ components/ExercisePageInput.tsx
@@ -9,7 +9,25 @@ interface Props {
 export const ExercisePageInput = ({ displayPrecision = 2, numberChangedCallback }: Props) => {
   const [input, setInput] = useState<string>((0).toFixed(displayPrecision));
 
-  function onEnterText(newText: string) {
+  /**
+   * Partially validate input, so the user can not put letters or other stuff in there.
+   * But don't trigger numberChangedCallback yet
+   * @param newValue New string Value
+   */
+  function onInputChanged(newValue: string) {
+    if (Number.isNaN(parseFloat(newValue))) {
+      // Don't update input if its invalid
+      return;
+    }
+
+    setInput(newValue);
+  }
+
+  /**
+   * Parses input and triggers numberChangedCallback when user is done editing
+   * @param newText Text to be parsed
+   */
+  function onSubmit(newText: string) {
     const asNumber = parseFloat(newText.trim());
     if (Number.isNaN(asNumber)) {
       // Don't update input if its invalid
@@ -22,7 +40,6 @@ export const ExercisePageInput = ({ displayPrecision = 2, numberChangedCallback 
 
     setInput(asNumber.toFixed(displayPrecision));
   }
-
   return (
     <TextInput
       className="w-full rounded-md border-2 border-gray-400 p-4 text-base"
@@ -30,8 +47,9 @@ export const ExercisePageInput = ({ displayPrecision = 2, numberChangedCallback 
       underlineColorAndroid={'transparent'}
       autoCorrect={false}
       value={input}
-      onChangeText={newText => setInput(newText)}
-      onBlur={() => onEnterText(input)}
+      onChangeText={onInputChanged}
+      onBlur={() => onSubmit(input)}
+      submitBehavior={'blurAndSubmit'}
     />
   );
 };
