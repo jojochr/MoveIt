a1deb893917cc52cfaca2c60f7940056f715c357
Johannes Christel <johannes.christel@outlook.de>
2025-07-17 19:46:09 +0200

    Added increment and decrement buttons to ExercisePageInput Component

--- components/ExercisePage/ExercisePageInput.tsx
+++ components/ExercisePage/ExercisePageInput.tsx
@@ -1,14 +1,20 @@
 import { TextInput } from 'react-native-gesture-handler';
 import { useState } from 'react';
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 
 interface Props {
   displayPrecision: number;
   numberChangedCallback?: (newValue: number, isValid: boolean) => void;
   unitText?: string;
+  showIncrementDecrementButtons?: boolean;
 }
 
-export const ExercisePageInput = ({ displayPrecision = 2, numberChangedCallback, unitText }: Props) => {
+export const ExercisePageInput = ({
+  displayPrecision = 2,
+  numberChangedCallback,
+  unitText,
+  showIncrementDecrementButtons = true,
+}: Props) => {
   const [input, setInput] = useState<string>((0).toFixed(displayPrecision));
   const [isValid, setIsValid] = useState(true);
 
@@ -29,7 +35,7 @@ export const ExercisePageInput = ({ displayPrecision = 2, numberChangedCallback,
   /**
    * This method just makes it look a little nicer on submit, if the number is actually valid
    */
-  function prettify() {
+  function onSubmit(input: string) {
     const asNumber = parseFloat(input.trim());
 
     if (Number.isNaN(asNumber)) return;
@@ -48,11 +54,60 @@ export const ExercisePageInput = ({ displayPrecision = 2, numberChangedCallback,
         autoCorrect={false}
         value={input}
         onChangeText={onChange}
-        onBlur={() => prettify()}
+        onBlur={() => onSubmit(input)}
         submitBehavior={'blurAndSubmit'}
       />
 
       {unitText && <Text>{unitText}</Text>}
+
+      {showIncrementDecrementButtons && (
+        <IncrementDecrementButtons
+          className="flex flex-1 items-end"
+          onIncrement={() => {
+            onSubmit(OperateIfNumber(input, { type: 'Increment' }));
+          }}
+          onDecrement={() => {
+            onSubmit(OperateIfNumber(input, { type: 'Decrement' }));
+          }}
+        />
+      )}
+    </View>
+  );
+};
+
+type Operations = { type: 'Increment' } | { type: 'Decrement' };
+
+function OperateIfNumber(inputStr: string, operation: Operations): string {
+  const asNumber = parseFloat(inputStr.trim());
+  if (Number.isNaN(asNumber)) return inputStr;
+
+  switch (operation.type) {
+    case 'Increment':
+      return (asNumber + 1).toString();
+    case 'Decrement':
+      return (asNumber - 1).toString();
+  }
+}
+
+export const IncrementDecrementButtons = ({
+  onIncrement,
+  onDecrement,
+  className = '',
+}: {
+  className?: string;
+  onIncrement?: () => void;
+  onDecrement?: () => void;
+}) => {
+  return (
+    <View className={className}>
+      <View className="w-fit">
+        <Pressable onPress={onIncrement} className="rounded-t-full border-2 px-2" style={{ borderBottomWidth: 1 }}>
+          <Text className="text-center text-2xl font-black">+</Text>
+        </Pressable>
+        <Pressable onPress={onDecrement} className="rounded-b-full border-2" style={{ borderTopWidth: 1 }}>
+          <Text className="text-center text-2xl font-black">—</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
